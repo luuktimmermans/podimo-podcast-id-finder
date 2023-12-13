8b65317ad3617535dc05b6ad145eedd11aade933
luuktimmermans <lwamtimmermans@gmail.com>
2023-12-13 01:26:45 +0100

    Finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import htmx from "astro-htmx";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
