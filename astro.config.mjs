a40c14f313f83df17a4c0d78250f52c64605b997
luuktimmermans <lwamtimmermans@gmail.com>
2024-01-22 23:26:33 +0100

    changed font sizing on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import htmx from "astro-htmx";
 import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), htmx()],
-  adapter: vercel()
+  adapter: vercel(),
 });
