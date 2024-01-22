585a45eddc7e10ac111007d907c91de7be1fb758
luuktimmermans <lwamtimmermans@gmail.com>
2024-01-22 21:46:24 +0100

    Fixed loading state

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import tailwind from "@astrojs/tailwind";
 import htmx from "astro-htmx";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), htmx()],
-  site: "https://luuktimmermans.github.io",
-  base: "/podimo-podcast-id-finder",
+  adapter: vercel()
 });
